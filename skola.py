03b500d49fee9da017d638a965dd4a43f885da38
henrietei <113431841+henrietei@users.noreply.github.com>
2022-09-21 14:58:51 +0300

    Create skola.py

---
+++ skola.py
@@ -0,0 +1,31 @@
+class Student:
+    name=None
+    marks=None
+
+
+    def __init__(self, name=None, marks=None):
+        self.name=name
+        self.marks=marks
+
+        self.get_data()
+
+    def set_data(self, name=None, marks=None):
+        self.name=name
+        self.marks=marks
+
+
+    def get_data(self):
+        print (self.name, 'atzime:', self.marks)
+
+    def check_pass_fail(self):
+        if self.marks>5:
+            print("pass")
+        else:
+            print("fail")
+
+
+
+student1=Student("henriete", 7)
+student2=Student("anna", 8)
+check1=student1.check_pass_fail
+print(check1)
